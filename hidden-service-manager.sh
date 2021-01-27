3c7a5fb059532cee7af3a8dde8274fdfd17b2500
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-27 06:01:54 -0500

    Create hidden-service-manager.sh

---
+++ hidden-service-manager.sh
@@ -0,0 +1,50 @@
+#!/bin/bash
+# https://github.com/complexorganizations/hidden-service-manager
+
+# Require script to be run as root
+function super-user-check() {
+  if [ "$EUID" -ne 0 ]; then
+    echo "You need to run this script as super user."
+    exit
+  fi
+}
+
+# Check for root
+super-user-check
+
+# Detect Operating System
+function dist-check() {
+  if [ -e /etc/os-release ]; then
+    # shellcheck disable=SC1091
+    source /etc/os-release
+    DISTRO=$ID
+    DISTRO_VERSION=$VERSION_ID
+  fi
+}
+
+# Check Operating System
+dist-check
+
+# Pre-Checks system requirements
+function installing-system-requirements() {
+  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ] || [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ] || [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ] || [ "$DISTRO" == "alpine" ]; }; then
+    if { ! [ -x "$(command -v curl)" ] || ! [ -x "$(command -v iptables)" ] || ! [ -x "$(command -v bc)" ] || ! [ -x "$(command -v jq)" ] || ! [ -x "$(command -v sed)" ] || ! [ -x "$(command -v zip)" ] || ! [ -x "$(command -v unzip)" ] || ! [ -x "$(command -v grep)" ] || ! [ -x "$(command -v awk)" ] || ! [ -x "$(command -v ip)" ]; }; then
+      if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ]; }; then
+        apt-get update && apt-get install iptables curl coreutils bc jq sed e2fsprogs zip unzip grep gawk iproute2 -y
+      elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+        yum update -y && yum install epel-release iptables curl coreutils bc jq sed e2fsprogs zip unzip grep gawk iproute2 -y
+      elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+        pacman -Syu --noconfirm iptables curl bc jq sed zip unzip grep gawk iproute2
+      elif [ "$DISTRO" == "alpine" ]; then
+        apk update && apk add iptables curl bc jq sed zip unzip grep gawk iproute2
+      fi
+    fi
+  else
+    echo "Error: $DISTRO not supported."
+    exit
+  fi
+}
+
+# Run the function and check for requirements
+installing-system-requirements
+
