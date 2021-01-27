d43233ac70d66b2e329f6c8b1ae466a1e28efb30
Prajwal Koirala <16564273+Prajwal-Koirala@users.noreply.github.com>
2021-01-27 10:48:41 -0500

    Update hidden-service-manager.sh

--- hidden-service-manager.sh
+++ hidden-service-manager.sh
@@ -53,18 +53,8 @@ TOR_PATH="/etc/tor"
 TOR_TORRC="$TOR_PATH/torrc"
 HIDDEN_SERVICE_MANAGER="$TOR_PATH/hidden-service-manager"
 HIDDEN_SERVICE_MANAGER_UPDATE="https://raw.githubusercontent.com/complexorganizations/hidden-service-manager/main/hidden-service-manager.sh"
-
-# Verify that it is an old installation or another installer
-function previous-tor-installation() {
-  if [ -d "$TOR_PATH" ]; then
-    if [ ! -f "$HIDDEN_SERVICE_MANAGER" ]; then
-      rm -rf $TOR_PATH
-    fi
-  fi
-}
-
-# Run the function to eliminate old installation or another installer
-# previous-tor-installation
+NGINX_GLOBAL_CONFIG="/etc/nginx/nginx.conf"
+FAIL_TO_BAN_CONFIG="/etc/fail2ban/jail.conf"
 
 if [ ! -f "$TOR_TORRC" ]; then
 
@@ -88,45 +78,49 @@ if [ ! -f "$TOR_TORRC" ]; then
 
   install-service
 
-function secure-firewall() {
-  # Install Firwall
-  if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ]; }; then
-    apt-get update
-    apt-get install ufw fail2ban -y
-  elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
-    yum install ufw -y
-  elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
-    pacman -Syu --noconfirm ufw fail2ban
-  elif [ "$DISTRO" == "alpine" ]; then
-    apk install ufw fail2ban
-  fi
-  # Configure UFW
-  if [ -x "$(command -v ufw)" ]; then
-    ufw allow 80/tcp
-    ufw default reject incoming
-    ufw default reject outgoing
-  fi
-  # Secure Nginx
-  if [ -x "$(command -v nginx)" ]; then
-    sed -i "s|# server_tokens off;|server_tokens off;|" /etc/nginx/nginx.conf
-    chmod 700 /var/www/html/
-  fi
-  # Fail2ban
-  if [ ! -f "/etc/fail2ban/jail.conf" ]; then
-    sed -i "s|# bantime = 1h|bantime = 720h|" /etc/fail2ban/jail.conf
-    sed -i "s|# enabled = true|enabled = true|" /etc/fail2ban/jail.conf
-  fi
-}
+  function install-firewall() {
+    # Install Firwall
+    if { [ "$DISTRO" == "ubuntu" ] || [ "$DISTRO" == "debian" ] || [ "$DISTRO" == "raspbian" ] || [ "$DISTRO" == "pop" ] || [ "$DISTRO" == "kali" ]; }; then
+      apt-get update
+      apt-get install ufw fail2ban -y
+    elif { [ "$DISTRO" == "fedora" ] || [ "$DISTRO" == "centos" ] || [ "$DISTRO" == "rhel" ]; }; then
+      yum install ufw -y
+    elif { [ "$DISTRO" == "arch" ] || [ "$DISTRO" == "manjaro" ]; }; then
+      pacman -Syu --noconfirm ufw fail2ban
+    elif [ "$DISTRO" == "alpine" ]; then
+      apk install ufw fail2ban
+    fi
+  }
 
-function configure-nginx() {
-  # Secure Nginx
-  if [ -x "$(command -v nginx)" ]; then
-    sed -i "s|listen 80 default_server;|listen 8080 default_server;|" /etc/nginx/sites-enabled/default
-    sed -i "s|listen [::]:80 default_server;|listen [::]:8080 default_server;|" /etc/nginx/sites-enabled/default
-    sed -i "s|#HiddenServiceDir /var/lib/tor/hidden_service/|HiddenServiceDir /var/lib/tor/hidden_service/|" /etc/tor/torrc
-    sed -i "s|#HiddenServicePort 80 127.0.0.1:80|HiddenServicePort 80 127.0.0.1:8080|" /etc/tor/torrc
-  fi
-}
+  install-firewall
+
+  function configure-service() {
+    # Secure Nginx
+    if [ -x "$(command -v nginx)" ]; then
+      sed -i "s|listen 80 default_server;|listen 8080 default_server;|" /etc/nginx/sites-enabled/default
+      sed -i "s|listen [::]:80 default_server;|listen [::]:8080 default_server;|" /etc/nginx/sites-enabled/default
+      sed -i "s|#HiddenServiceDir /var/lib/tor/hidden_service/|HiddenServiceDir /var/lib/tor/hidden_service/|" /etc/tor/torrc
+      sed -i "s|#HiddenServicePort 80 127.0.0.1:80|HiddenServicePort 80 127.0.0.1:8080|" /etc/tor/torrc
+    fi
+    # Configure UFW
+    if [ -x "$(command -v ufw)" ]; then
+      ufw allow 80/tcp
+      ufw allow 22/tcp
+      ufw default reject incoming
+      ufw default reject outgoing
+    fi
+    # Secure Nginx
+    if [ -x "$(command -v nginx)" ]; then
+      sed -i "s|# server_tokens off;|server_tokens off;|" $NGINX_GLOBAL_CONFIG
+    fi
+    # Fail2ban
+    if [ ! -f "$FAIL_TO_BAN_CONFIG" ]; then
+      sed -i "s|# bantime = 1h|bantime = 720h|" $FAIL_TO_BAN_CONFIG
+      sed -i "s|# enabled = true|enabled = true|" $FAIL_TO_BAN_CONFIG
+    fi
+  }
+
+  configure-service
 
   function restart-service() {
     if pgrep systemd-journal; then
@@ -157,9 +151,9 @@ function configure-nginx() {
       service fail2ban restart
     fi
   }
-  
+
   restart-service
-  
+
   HOSTNAME=$(cat /var/lib/tor/hidden_service/hostname)
   echo "Your Hostname: $HOSTNAME"
 
